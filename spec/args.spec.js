b57619236a92755f7dbef88f9f7f2f43036de2a2
Wang Qiu <winsonwq@gmail.com>
2012-07-04 01:11:43 +0800

    add tests for toAbsPathFiles and foldersToFiles methods in args.js file

--- spec/args.spec.js
+++ spec/args.spec.js
@@ -1,3 +1,6 @@
+var sinon = require('sinon');
+var args = require('../lib/args');
+
 describe("args", function() {
 	var should = require('chai').should();
 	var process;
@@ -8,41 +11,69 @@ describe("args", function() {
 		};
 	});
 
-	[
-		['/User/yourname/nodeproj', 'spec'],
-		['/User/yourname/nodeproj', 'spec/a.js', 'spec/inner/b.js', 'inner'],
-		['', 'spec'],
-		['', '']
-	].forEach(function(item) {
-		it("should return project folder : '" + item[0] + "' and specFolder : '" + item.slice(1).join(',') + "'", function(done) {
-			var specInfo = item.slice(1);
-			var specFiles = specInfo.filter(function(item){ 
-				return item.indexOf('.js') !== -1; 
+	describe("#getParams", function() {
+
+		[
+			['/User/yourname/nodeproj', 'spec'],
+			['/User/yourname/nodeproj', 'spec/a.js', 'spec/inner/b.js', 'inner'],
+			['', 'spec'],
+			['', '']
+		].forEach(function(item) {
+			it("should return project folder : '" + item[0] + "' and specFolder : '" + item.slice(1).join(',') + "'", function(done) {
+				var specInfo = item.slice(1);
+				var specFiles = specInfo.filter(function(item) {
+					return item.indexOf('.js') !== -1;
+				});
+
+				var specFolders = specInfo.filter(function(item) {
+					return item.indexOf('.js') === -1;
+				});
+
+				process.argv.push(item[0]);
+				process.argv = process.argv.concat(item.slice(1));
+				process.argv.push('+');
+				process.argv.push('-R');
+				process.argv.push('html-cov');
+
+				var argument = args.getParams(process.argv.slice(2));
+				argument.projectFolder.should.equal(item[0]);
+				argument.params.should.eql(['-R', 'html-cov']);
+				argument.isCoverageReport.should.be.true;
+				done();
 			});
+		});
 
-			var specFolders = specInfo.filter(function(item){ 
-				return item.indexOf('.js') === -1;
-			});
+		it("should return empty project and spec folder if there is no params", function(done) {
+			var argument = args.getParams(process.argv.slice(2));
+			argument.projectFolder.should.equal('');
+			argument.params.should.eql([]);
+			argument.isCoverageReport.should.not.be.true;
+			done();
+		});
 
-			process.argv.push(item[0]);
-			process.argv = process.argv.concat(item.slice(1));
-			process.argv.push('+');
-			process.argv.push('-R');
-			process.argv.push('html-cov');
+	});
 
-			var args = require('../lib/args').getParams(process.argv.slice(2));
-			args.projectFolder.should.equal(item[0]);
-			args.params.should.eql(['-R', 'html-cov']);
-			args.isCoverageReport.should.be.true;
+	describe("#foldersToFiles", function() {
+		var fs = require('fs');
+
+		it("should return specific js files in the spec folders based on a basePath", function(done) {
+			var readDirStub = sinon.stub(fs, 'readdirSync').returns(['a.js', 'b.js', 'c.txt']);
+			var files = args.foldersToFiles(['spec', 'spec/inner'], '/Users');
+
+			files.should.eql(['/Users/spec/a.js', '/Users/spec/b.js', '/Users/spec/inner/a.js', '/Users/spec/inner/b.js']);
+			readDirStub.restore();
 			done();
 		});
 	});
 
-	it("should return empty project and spec folder if there is no params", function(done) {
-		var args = require('../lib/args').getParams(process.argv.slice(2));
-		args.projectFolder.should.equal('');
-		args.params.should.eql([]);
-		args.isCoverageReport.should.not.be.true;
-		done();
+	describe("#toAbsPathFiles", function() {
+
+		it("should return specific js files based on a basePath", function(done) {
+			var files = args.toAbsPathFiles(['spec/a.js', 'spec/inner/b.js'], '/Users');
+			files.should.eql(['/Users/spec/a.js', '/Users/spec/inner/b.js']);
+			done();
+		});
+
 	});
+
 });
